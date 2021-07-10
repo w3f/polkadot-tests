4c44aa06bb78492a7573ea5005c6426e5488f9c4
Florian Franzen <Florian.Franzen@gmail.com>
2021-07-10 16:36:01 +0200

    kagome-adapter: Fix return code on assert

--- adapters/kagome/src/assert.cpp
+++ adapters/kagome/src/assert.cpp
@@ -1,21 +1,29 @@
 #include "assert.hpp"
 
+#include <sstream>
+
 namespace boost {
 
   void assertion_failed(char const * expr, char const * function, char const * file, long line) {
-    std::cout << "Assert inside function '" << function
-              << "' in file '" << file
-              << "' on line '" << line
-              << "' when evaluating '" << expr
-              << "'" << std::endl;
+    std::stringstream ss;
+    ss << "Assert inside function '" << function
+       << "' in file '" << file
+       << "' on line '" << line
+       << "' when evaluating '" << expr
+       << "'";
+
+    throw std::runtime_error(ss.str());
   }
 
   void assertion_failed_msg(char const * expr, char const * msg, char const * function, char const * file, long line) {
-    std::cout << "Error inside function '" << function
-              << "' in file '" << file
-              << "' on line '" << line
-              << "' when evaluating '" << expr
-              << "': " << msg << std::endl;
+    std::stringstream ss;
+    ss << "Error inside function '" << function
+       << "' in file '" << file
+       << "' on line '" << line
+       << "' when evaluating '" << expr
+       << "': " << msg;
+
+    throw std::runtime_error(ss.str());
   }
 
 } // namespace boost
