8d588ffb34a8411b06ecc75cc9bc135c8b2e213b
Florian Franzen <Florian.Franzen@gmail.com>
2021-07-10 16:36:01 +0200

    kagome-adapter: Fix return code on assert

--- adapters/kagome/src/main.cpp
+++ adapters/kagome/src/main.cpp
@@ -38,53 +38,57 @@
  * Tree compatibility tests
  */
 int main(int argc, char **argv) {
-  // Initialize logging system
-  auto loglevel = kagome::log::Level::ERROR;
-  if (argc > 1 and argv[1] == std::string_view("--verbose")) {
-    loglevel = kagome::log::Level::DEBUG;
-    argc--;
-    argv++;
-  }
-
-  auto config = std::make_shared<soralog::FallbackConfigurator>(loglevel);
-  auto logger = std::make_shared<soralog::LoggingSystem>(config);
-
-  auto result = logger->configure();
-  auto e0 = "Failed to configure logger: " + result.message;
-  BOOST_ASSERT_MSG(!result.has_error, e0.data());
-
-  kagome::log::setLoggingSystem(logger);
-
-  // Initialize subcommand router with known commands
-  SubcommandRouter<int, char **> router;
-  router.addSubcommand("scale-codec", [](int argc, char **argv) {
-    processScaleCodecCommand(extractScaleArgs(argc, argv));
-  });
-  router.addSubcommand("state-trie", [](int argc, char **argv) {
-    processTrieCommand(extractTrieArgs(argc, argv));
-  });
-  router.addSubcommand("host-api", [](int argc, char **argv) {
-    host_api::processCommand(host_api::extractArgs(argc, argv));
-  });
-
-  // Check if subcommand is provided
-  std::string commands_list = "Valid subcommands are: ";
-  for (auto &&name : router.collectSubcommandNames()) {
-    commands_list += name;
-    commands_list += " ";
-  }
-
-  auto e1 = "Subcommand is not provided\n" + commands_list;
-  BOOST_ASSERT_MSG(argc > 1, e1.data());
-
-  // Try to run subcommand
   try {
+    // Initialize logging system
+    auto loglevel = kagome::log::Level::ERROR;
+    if (argc > 1 and argv[1] == std::string_view("--verbose")) {
+      loglevel = kagome::log::Level::DEBUG;
+      argc--;
+      argv++;
+    }
+
+    auto config = std::make_shared<soralog::FallbackConfigurator>(loglevel);
+    auto logger = std::make_shared<soralog::LoggingSystem>(config);
+
+    auto result = logger->configure();
+    auto e0 = "Failed to configure logger: " + result.message;
+    BOOST_ASSERT_MSG(!result.has_error, e0.data());
+
+    kagome::log::setLoggingSystem(logger);
+
+    // Initialize subcommand router with known commands
+    SubcommandRouter<int, char **> router;
+    router.addSubcommand("scale-codec", [](int argc, char **argv) {
+      processScaleCodecCommand(extractScaleArgs(argc, argv));
+    });
+    router.addSubcommand("state-trie", [](int argc, char **argv) {
+      processTrieCommand(extractTrieArgs(argc, argv));
+    });
+    router.addSubcommand("host-api", [](int argc, char **argv) {
+      host_api::processCommand(host_api::extractArgs(argc, argv));
+    });
+
+    // Check if subcommand is provided
+    std::string commands_list = "Valid subcommands are: ";
+    for (auto &&name : router.collectSubcommandNames()) {
+      commands_list += name;
+      commands_list += " ";
+    }
+
+    auto e1 = "Subcommand is not provided\n" + commands_list;
+    BOOST_ASSERT_MSG(argc > 1, e1.data());
+
+    // Run subcommand
     auto e2 = "Invalid subcommand\n" + commands_list;
     BOOST_VERIFY_MSG(router.executeSubcommand(argv[1], argc - 1, argv + 1),
                      e2.data());
-  } catch (const NotImplemented &e) {
+
+  } catch (const NotImplemented& e) {
     return EOPNOTSUPP;
+  } catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
+    return EXIT_FAILURE;
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
