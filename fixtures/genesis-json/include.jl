4a11515f13aca9bedc59b4524cbbe5dfdccb5ab2
Florian Franzen <Florian.Franzen@gmail.com>
2021-12-21 17:52:44 +0100

    fixtures: initial commit of genesis-json

---
+++ fixtures/genesis-json/include.jl
@@ -0,0 +1,23 @@
+using .HostFixture
+using Test
+
+
+tester = HostFixture.Tester("JSON Genesis", false, "tester")
+
+HostFixture.execute(tester, 10) do (root, result)
+    # Extract all hashes returned from log
+    hashes = map(m -> m[1], eachmatch(r"##([^#\n]+)##", result)) 
+
+    # Check state root hash
+    @test root == hashes[1]
+
+
+    # Extract all calls made from log
+    calls = map(m -> m[1], eachmatch(r"@@([^@\n]+)@@", result))
+
+    # Check that grandpa config is requested
+    @test "grandpa_authorities()" in calls
+
+    # Check that babe configuration is requested
+    @test "configuration()" in calls
+end
