5a02e297af37c1a6af3b0bcd6484c31618a5e98d
lamafab <42901763+lamafab@users.noreply.github.com>
2022-03-14 16:59:54 +0100

    expand blanket implementation, get rid of warnings

--- adapters/substrate/src/host_api/offchain.rs
+++ adapters/substrate/src/host_api/offchain.rs
@@ -1,13 +1,13 @@
-use crate::host_api::utils::{str, ParsedInput, Runtime};
+use crate::host_api::utils::{ParsedInput, Runtime};
 use parity_scale_codec::{Encode, Decode};
 
 #[derive(Debug, Decode)]
 struct OpaqueNetworkState {
-	peer_id: Vec<u8>,
-	addresses: Vec<Vec<u8>>,
+	_peer_id: Vec<u8>,
+	_addresses: Vec<Vec<u8>>,
 }
 
-pub fn ext_offchain_is_validator_version_1(mut rtm: Runtime) {
+pub fn ext_offchain_is_validator_version_1(mut rtm: Runtime, _input: ParsedInput) {
 	let res = rtm.call_and_decode::<bool>(
 		"rtm_ext_offchain_is_validator_version_1", &[]
 	);
@@ -16,11 +16,11 @@ pub fn ext_offchain_is_validator_version_1(mut rtm: Runtime) {
 	println!("{}", res);
 }
 
-pub fn ext_offchain_submit_transaction_version_1(mut rtm: Runtime) {
+pub fn ext_offchain_submit_transaction_version_1(_rtm: Runtime, _input: ParsedInput) {
 
 }
 
-pub fn ext_offchain_network_state_version_1(mut rtm: Runtime) {
+pub fn ext_offchain_network_state_version_1(mut rtm: Runtime, _input: ParsedInput) {
 	let res = rtm.call_and_decode::<Result<OpaqueNetworkState, ()>>(
 		"rtm_ext_offchain_is_validator_version_1", &[]
 	);
@@ -29,7 +29,7 @@ pub fn ext_offchain_network_state_version_1(mut rtm: Runtime) {
 	println!("{:?}", res);
 }
 
-pub fn ext_offchain_timestamp_version_1(mut rtm: Runtime) {
+pub fn ext_offchain_timestamp_version_1(mut rtm: Runtime, _input: ParsedInput) {
 	let res = rtm.call_and_decode::<u64>(
 		"rtm_ext_offchain_timestamp_version_1", &[]
 	);
@@ -38,7 +38,7 @@ pub fn ext_offchain_timestamp_version_1(mut rtm: Runtime) {
 	println!("{:?}", res);
 }
 
-pub fn ext_offchain_sleep_until_version_1(mut rtm: Runtime) {
+pub fn ext_offchain_sleep_until_version_1(mut rtm: Runtime, _input: ParsedInput) {
 	use std::time::{SystemTime, UNIX_EPOCH};
 
 	// Sleep for 5 seconds.
@@ -48,51 +48,51 @@ pub fn ext_offchain_sleep_until_version_1(mut rtm: Runtime) {
 		.as_millis()
 		+ 5_000;
 
-	let res = rtm.call_and_decode::<()>(
+	let _res = rtm.call_and_decode::<()>(
 		"rtm_ext_offchain_sleep_until_version_1", &(deadline).encode()
 	);
 }
 
-pub fn ext_offchain_random_seed_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_random_seed_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_local_storage_set_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_local_storage_set_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_local_storage_clear_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_local_storage_clear_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_local_storage_compare_and_set_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_local_storage_compare_and_set_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_local_storage_get_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_local_storage_get_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_http_request_start_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_http_request_start_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_http_request_add_header_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_http_request_add_header_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_http_request_write_body_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_http_request_write_body_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_http_response_wait_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_http_response_wait_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_http_response_headers_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_http_response_headers_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
 
-pub fn ext_offchain_http_response_read_body_version_1(mut rtm: Runtime) {
-
+pub fn ext_offchain_http_response_read_body_version_1(_rtm: Runtime, _input: ParsedInput) {
+	unimplemented!()
 }
