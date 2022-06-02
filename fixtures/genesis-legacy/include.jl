635dfa4df7ce982780ff35b375b53d69227b56b2
Florian Franzen <Florian.Franzen@gmail.com>
2022-06-02 14:42:07 +0200

    ci: disable genesis-default for kagome & gossamer

--- fixtures/genesis-legacy/include.jl
+++ fixtures/genesis-legacy/include.jl
@@ -10,4 +10,14 @@ HostFixture.execute(tester, 10) do (root, result)
 
     # Check state root hash
     @test root == hashes[1]
+
+
+    # Extract all calls made from log
+    calls = map(m -> m[1], eachmatch(r"@@([^@\n]+)@@", result))
+
+    # Check that grandpa config is requested
+    @test "grandpa_authorities()" in calls
+
+    # Check that babe configuration is requested
+    @test "configuration()" in calls
 end
